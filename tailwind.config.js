5ccaef652f32cbd8dc6da0469f61b0cb9f2231ea
Fitri Rahim <fitrialrahim@gmail.com>
2023-07-06 00:40:02 +0800

    remove tailwind retest by importing css thru js and working

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./themes/zero/templates/*.html","./output/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
