dddd20c0d9bcbcab870ebfac6d55925fe7f52bac
Fitri Rahim <fitrialrahim@gmail.com>
2023-07-05 23:42:57 +0800

    add tailwindcss thru cdn ignore the main.js and base.css not working right now

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./themes/zero/templates/*.html","./output/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
