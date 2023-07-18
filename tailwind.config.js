54c886487fff02748378975478db255f805cca93
Fitri Rahim <fitri@fitri.me>
2023-07-18 15:29:47 +0800

    remove all style and use typography from tailwind itself

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./themes/zero/templates/*.html"],
   theme: {
-    extend: {
-      fontFamily: {
-        custom: ['Montserrat', 'sans-serif']
-      },
-      fontWeight: {
-        regular: 400,
-        medium: 500,
-        semibold: 600,
-        extrabold:800,
-        black:900
-        },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            'h1': {
-              '@apply mr-5': {},
-            }
-          }
-        }
-      })
-    }
+    extend: {}
   },
-  plugins: []
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
 };
